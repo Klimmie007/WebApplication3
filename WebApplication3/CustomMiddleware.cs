26b475a82f82c9cd5214d514ae97c28252f51b03
Kamil Milewski <kamilewski@yahoo.com>
2021-04-20 18:11:53 +0200

    Middleware

---
+++ WebApplication3/CustomMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+
+namespace WebApplication3
+{
+    public class CustomMiddleware
+    {
+        private readonly RequestDelegate next;
+        public CustomMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public Task Invoke(HttpContext httpContext)
+        {
+            if (httpContext.Request.Headers["User-Agent"].ToString() == "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36 Edg/90.0.818.41")
+            {
+                httpContext.Response.WriteAsync("Przegladarka nie jest obslugiwana");
+            }
+            return next(httpContext);
+        }
+    }
+}
--- WebApplication3/Startup.cs
+++ WebApplication3/Startup.cs
@@ -52,7 +52,7 @@ namespace WebApplication3
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            app.UseMiddleware<CustomMiddleware>();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
